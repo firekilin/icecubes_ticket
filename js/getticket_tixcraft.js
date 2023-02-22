1575d54076b68ef6ee0032c1ff4239df2706116a
kilin <wl00161839@gmail.com>
2023-02-22 11:32:36 +0800

    最新

--- js/getticket_tixcraft.js
+++ js/getticket_tixcraft.js
@@ -59,13 +59,17 @@ $(()=>{
         "tixcraft_autosend",
         "tixcraft_ticketcount"
       ] ,(result)=> {
-        
+        $("a").each((k,e)=>{
+        if($(e).text()=="立即購票"){
+            $(e).parent().parent().append($("<a>").attr("href",$(e).attr("href")).text("冰塊:這網址更快"));
+        }});
+        let iceconsole=$("<div>").attr("style","width: 200px;");
         let icediv = document.createElement("div");
         icediv.setAttribute("id", "icecubes_ticket");
         let iceimg =document.createElement("img");
         if(result.tixcraft_date==null||result.tixcraft_date==''){
             result.tixcraft_quick=false;
-            icediv.append("未輸入日期");
+            iceconsole.text("未輸入日期");
         }
 
         if(result.tixcraft_quick){
@@ -90,12 +94,13 @@ $(()=>{
           
         }
         icediv.append(iceimg);
+        $(icediv).append(iceconsole);
         $("body").append(icediv);
         if(result.tixcraft_quick){
             
             let restartticket=()=>{
-                $("a[class='btn btn-default btn-lg']").each((k,e)=>{
-                  if($(e).text()=="立即購票"){
+                $("a").each((k,e)=>{
+                    if($(e).text()=="立即購票"){
                         let element=e;
                         if ("createEvent" in document) {
                             var evt = document.createEvent("HTMLEvents");
@@ -105,33 +110,23 @@ $(()=>{
                         else{
                             element.fireEvent("click");
                         }
-                  }});
+                    }});
             }
-            $("a[class='btn btn-default btn-lg']").each((k,e)=>{
-                if($(e).text()=="立即購票"){
-                    let element=e;
-                    if ("createEvent" in document) {
-                        var evt = document.createEvent("HTMLEvents");
-                        evt.initEvent("click", false, true);
-                        element.dispatchEvent(evt);
-                    }
-                    else{
-                        element.fireEvent("click");
-                    }
-                    
-                    
-                }
-            });
+            console.log(window.location.href);
+
             if(window.location.href.indexOf("activity/detail")>0){
+                restartticket();
+                let trygettime=0;
                 let tryget = setInterval(function() {
+                    trygettime+=1;
                     let showlist=$("tr[class='gridc fcTxt']");
                     if(showlist.length>0){
                         showlist.each((k,e)=>{
-                            if($(e).text().includes(result.tixcraft_date.replaceAll("-","/"))&&
+                            if($(e).text().replaceAll("-","/").includes(result.tixcraft_date.replaceAll("-","/"))&&
                                 $(e).text().includes(result.tixcraft_time)){
                                     
-                                    if($(e).find("input").length>0){
-                                        let element=$(e).find("input")[0];
+                                    if($(e).find("button").length>0){
+                                        let element=$(e).find("button")[0];
                                         if ("createEvent" in document) {
                                             var evt = document.createEvent("HTMLEvents");
                                             evt.initEvent("click", false, true);
@@ -146,23 +141,23 @@ $(()=>{
                                         console.log($(e).text());
                                     }
                             }
-                                
                         });
                     }else{
                         restartticket();
+                        
                         console.log($(showlist).text());
                     }
+                    iceconsole.text("(注意)已刷新次數:"+trygettime);
+                    
                 },500);
             }else if(window.location.href.indexOf("activity/game")>0){
-                let tryget = setInterval(function() {
                     let showlist=$("tr[class='gridc fcTxt']");
                     if(showlist.length>0){
                         showlist.each((k,e)=>{
-                            if($(e).text().includes(result.tixcraft_date.replaceAll("-","/"))&&
-                                $(e).text().includes(result.tixcraft_time)){
-                                    
-                                    if($(e).find("input").length>0){
-                                        let element=$(e).find("input")[0];
+                            if($(e).text().replaceAll("-","/").includes(result.tixcraft_date.replaceAll("-","/"))&&
+                                $(e).text().replaceAll(":","").includes(result.tixcraft_time.replaceAll(":",""))){
+                                    if($(e).find("button").length>0){
+                                        let element=$(e).find("button")[0];
                                         if ("createEvent" in document) {
                                             var evt = document.createEvent("HTMLEvents");
                                             evt.initEvent("click", false, true);
@@ -171,26 +166,16 @@ $(()=>{
                                         else{
                                             element.fireEvent("click");
                                         }
-                                        clearInterval(tryget);
                                     }else{
-                                        restartticket();
-                                        console.log($(e).text());
+                                        window.location.reload();
                                     }
                             }
                                 
                         });
                     }else{
-                        restartticket();
-                        console.log($(showlist).text());
+                        window.location.reload();
                     }
-                },500);
-            }
-           
-            if($("#checkCode").length>0){
-                $("#checkCode").val(result.tixcraft_anser);
-                $("#submitButton").click();
-            }
-            if($(".process-wizard-step.active").text().includes("選擇區域")){
+            }else if(window.location.href.indexOf("ticket/area")>0){ //選擇區域
                 if(result.tixcraft_auto=="human"){
                     
                     $("#select_form_manual").click();
@@ -202,26 +187,43 @@ $(()=>{
                         break;
                     }
                     if(arealist[i]!=""){
-                        $($("div[class='area_select col-md-5 col-xs-11 mg-top mg-c line-lf']").find("a")).each((k,e)=>{
-                            if($(e).text().replaceAll(" ","").replaceAll("區","").includes(arealist[i].replaceAll(" ","").replaceAll("區",""))){
-                                let element=document.getElementById($(e).attr("id"));
-                                if ("createEvent" in document) {
-                                    var evt = document.createEvent("HTMLEvents");
-                                    evt.initEvent("click", false, true);
-                                    element.dispatchEvent(evt);
-                                    findout=true;
+                        $($("div[class='zone area-list']").find("a")).each((k,e)=>{
+                            if($(e).text().includes("區")&&arealist[i].includes("區")){
+                                if($(e).text().replaceAll(" ","").includes(arealist[i].replaceAll(" ",""))){
+                                    let element=document.getElementById($(e).attr("id"));
+                                    if ("createEvent" in document) {
+                                        var evt = document.createEvent("HTMLEvents");
+                                        evt.initEvent("click", false, true);
+                                        element.dispatchEvent(evt);
+                                        findout=true;
+                                    }
+                                    else{
+                                        element.fireEvent("click");
+                                        findout=true;
+                                    }
                                 }
-                                else{
-                                    element.fireEvent("click");
-                                    findout=true;
+                            }else{
+                                if($(e).text().replaceAll(" ","").replaceAll("區","").includes(arealist[i].replaceAll(" ","").replaceAll("區",""))){
+                                    let element=document.getElementById($(e).attr("id"));
+                                    if ("createEvent" in document) {
+                                        var evt = document.createEvent("HTMLEvents");
+                                        evt.initEvent("click", false, true);
+                                        element.dispatchEvent(evt);
+                                        findout=true;
+                                    }
+                                    else{
+                                        element.fireEvent("click");
+                                        findout=true;
+                                    }
                                 }
                             }
+                            
                         });
                     }
                 }
                
                 if(result.tixcraft_omg&&!findout){
-                    $($("div[class='area_select col-md-5 col-xs-11 mg-top mg-c line-lf']").find("a")).each((k,e)=>{
+                    $($("div[class='zone area-list']").find("a")).each((k,e)=>{
                             let element=document.getElementById($(e).attr("id"));
                             if ("createEvent" in document) {
                                 var evt = document.createEvent("HTMLEvents");
@@ -234,20 +236,17 @@ $(()=>{
                     });
                 }
 
-            }else if($(".process-wizard-step.active").text().includes("選擇張數")){
-                
-
-
-
+            }else if(window.location.href.indexOf("ticket/ticket")>0){ //選擇張數
+                console.log("test");
                 if(result.tixcraft_auto!="human"){
-                    let maxcount= $($($("select[class=mobile-select]")[0]).find("option")[$($("select[class=mobile-select]")[0]).find("option").length-1]).text();
+                    let maxcount= $($("#ticketPriceList").find("select")[0]).find("option").length-1;
                     if(maxcount>=result.tixcraft_ticketcount){
-                        $($("select[class=mobile-select]")[0]).val(result.tixcraft_ticketcount);
+                        $($("#ticketPriceList").find("select")[0]).val(result.tixcraft_ticketcount);
                     }else{
-                        $($("select[class=mobile-select]")[0]).val(maxcount);
+                        $($("#ticketPriceList").find("select")[0]).val(maxcount);
                     }  
                 }
-                
+                /*
                 $("script").each((k,e)=>{
                     if($(e).text().includes("isTrust")){
                         let readline=$(e).text().split('"');
@@ -259,17 +258,18 @@ $(()=>{
                             }
                         }
                     }
-                });
+                });*/
+                $("label[for=TicketForm_agree]").click();
                 document.getElementById("TicketForm_verifyCode").focus();
                 let captchf=()=>{
-                    let captchaimg=$('#TicketForm').find('img');
-                    let captcha=$('#TicketForm').find('img').attr("src");
-                    if(captcha.includes('ticket/captcha')){
+                    let captchaimg=$('#TicketForm_verifyCode-image').find('img');
+                    let captcha=$('#TicketForm_verifyCode-image').attr("src");
+                    if(captcha.indexOf('ticket/captcha')>0){
                         getocrimg(captcha,(ocrstring)=>{
                             if(ocrstring.length==4){
                                 $('#TicketForm_verifyCode').val(ocrstring);
                                 if(result.tixcraft_autosend){
-                                    $("#ticketPriceSubmit").click();
+                                    $("#form-ticket-ticket").submit();
                                 }
                             }else{
                                 
@@ -285,6 +285,11 @@ $(()=>{
                 if(result.tixcraft_ocr){
                     captchf();
                 }
+            }
+           
+            if($("#checkCode").length>0){
+                $("#checkCode").val(result.tixcraft_anser);
+                $("#submitButton").click();
             }
             
         }
